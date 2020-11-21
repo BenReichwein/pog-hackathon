dbb3cca542f92b019a2e5b4e4033553ea71ba34b
Benjamin Reichwein <ben.basque@icloud.com>
2020-11-20 20:30:48 -0500

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login/Login'
+import ChatRooms from './components/Chatrooms/ChatRooms'
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <div className="App">
           <Switch>
-            <Route path="/" exact component={Login} />
+            <Route path="/" exact component={ChatRooms} />
             <Route path="/login" component={Login} />
           </Switch>
         </div>
